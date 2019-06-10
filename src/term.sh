0e4c4dee60fe941031da006483a918ca631c7190
Filipe Linhares <lip.linhares@gmail.com>
2019-06-10 12:05:47 -0300

    Add set_mongo_url alias

--- src/term.sh
+++ src/term.sh
@@ -3,7 +3,9 @@ alias docs="cht.sh"
 alias cat='bat'
 alias info='ls -lh'
 alias snill="subl ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Snippets/"
-
+set_mongo_url () {
+  export MONGO_URL=$1
+}
 
 delete-all-files () {
   find . -name $1 -type f -delete
